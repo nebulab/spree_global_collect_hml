56d22341ded802cd9c5fdde1ffd818a2d6d7833e
Matteo Latini <mtylty@gmail.com>
2015-04-21 16:32:43 +0200

    fixes params to be camelized like Global Collect wants

--- app/helpers/spree/admin/payments_helper_decorator.rb
+++ app/helpers/spree/admin/payments_helper_decorator.rb
@@ -1,13 +1,13 @@
 Spree::Admin::PaymentsHelper.module_eval do
   def global_collect_payment_url(payment)
     params = {
-      merchant_id: payment.payment_method.preferred_merchant_id,
-      order_id: payment.source.order_number,
-      effort_id: payment.source.effort_id,
-      attempt_id: payment.source.attempt_id,
-      payment_product_id: payment.source.payment_product_id,
-      payment_method_id: payment.source.payment_method_id,
-      payment_reference: payment.source.payment_reference
+      merchantId: payment.payment_method.preferred_merchant_id,
+      orderId: payment.source.order_number,
+      effortId: payment.source.effort_id,
+      attemptId: payment.source.attempt_id,
+      paymentProductId: payment.source.payment_product_id,
+      paymentMethodId: payment.source.payment_method_id,
+      paymentReference: payment.source.payment_reference
     }
 
     "https://wpc.gcsip.nl/wpc/generateOrderDetails.htm?#{params.to_query}"
