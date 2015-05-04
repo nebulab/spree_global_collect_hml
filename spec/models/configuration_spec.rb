c49ce19ffa09bb2ca7885316209066d0d6a9a7cd
Matteo Latini <mtylty@gmail.com>
2015-05-04 10:44:54 +0200

    fixes configuration spec

--- spec/models/configuration_spec.rb
+++ spec/models/configuration_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe Rails::Application::Configuration do
   subject { Rails.application.config.spree.payment_methods }
 
-  it { should include(Spree::Gateway::GlobalCollectHml::CreditCard) }
+  it { should include(Spree::Gateway::GlobalCollectHml) }
 end
